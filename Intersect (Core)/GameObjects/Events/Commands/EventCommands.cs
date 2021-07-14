039b64d263c8d94377e221ba62aea39294d7bcb7
Tamagotcheat <olivierj.83@hotmail.fr>
2021-07-14 17:24:18 +0200

    First modif on UseSpell command

--- Intersect (Core)/GameObjects/Events/Commands/EventCommands.cs
+++ Intersect (Core)/GameObjects/Events/Commands/EventCommands.cs
@@ -726,6 +726,15 @@ namespace Intersect.GameObjects.Events.Commands
 
     }
 
+    public class UseSpellCommand : EventCommand
+    {
+
+        public override EventCommandType Type { get; } = EventCommandType.UseSpell;
+
+        public Guid SpellId { get; set; }
+
+    }
+
     public class ShowPictureCommand : EventCommand
     {
 
--- Intersect (Core)/GameObjects/Events/Enums.cs
+++ Intersect (Core)/GameObjects/Events/Enums.cs
@@ -229,6 +229,8 @@
 
         //Reset Stats
         ResetStatPointAllocations,
+
+        UseSpell,
     }
 
 }
