0a6cf97f9bca4da2dcdbfd4de4736ec7035fcf4e
Tamagotcheat <olivierj.83@hotmail.fr>
2022-03-20 16:25:26 +0100

    Add fields and migrations script for the multitasking in quests

---
+++ Intersect.Server/Migrations/20220320150448_AddingQuestTaskProgressionToPlayers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intersect.Server.Migrations
+{
+    public partial class AddingQuestTaskProgressionToPlayers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TasksProgress",
+                table: "Player_Quests",
+                defaultValue: "{}",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TasksProgress",
+                table: "Player_Quests");
+        }
+    }
+}
