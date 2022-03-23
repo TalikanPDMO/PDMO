d6fdeef93e3398aaa6aa9ad7eec910ab2db92873
Tamagotcheat <olivierj.83@hotmail.fr>
2022-03-23 16:37:42 +0100

    Update the build to version 0.3.0

--- Intersect.Server/Properties/AssemblyInfo.cs
+++ Intersect.Server/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.2.0")]
-[assembly: AssemblyFileVersion("0.2.2.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
