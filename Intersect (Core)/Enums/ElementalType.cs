00a5f51998cc1498c3380eef6e454e4fcd80dc09
Tamagotcheat <olivierj.83@hotmail.fr>
2023-01-20 19:11:18 +0100

    Add the elemental types enumeration

---
+++ Intersect (Core)/Enums/ElementalType.cs
@@ -0,0 +1,46 @@
+namespace Intersect.Enums
+{
+
+    public enum ElementalType
+    {
+
+        None = 0,
+
+        Steel,
+
+        Fighting,
+
+        Dragon,
+
+        Water,
+
+        Electric,
+
+        Fairy,
+
+        Fire,
+
+        Ice,
+
+        Bug,
+
+        Normal,
+
+        Grass,
+
+        Poison,
+
+        Psychic,
+
+        Rock,
+
+        Ground,
+
+        Ghost,
+
+        Dark,
+
+        Flying
+    }
+
+}
