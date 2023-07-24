e741cf04d47a2322e4de186b47d4c18749867c90
Tamagotcheat <olivierj.83@hotmail.fr>
2023-07-24 15:06:29 +0200

    Update editor string for build 0.3.3.4

--- Intersect.Client/Properties/AssemblyInfo.cs
+++ Intersect.Client/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.3.3.4")]
-[assembly: AssemblyFileVersion("0.3.3.3")]
+[assembly: AssemblyFileVersion("0.3.3.4")]
 [assembly: NeutralResourcesLanguage("en-US")]
