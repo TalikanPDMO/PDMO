26b603f499076475a0c566b3b6551647328061a8
Jaauf <97916233+Jaauf@users.noreply.github.com>
2023-10-28 17:14:08 +0200

    New database attribute for itemcooldown

---
+++ Intersect.Server/Migrations/Game/20231008103331_AddingUseActiveSpellCooldown.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intersect.Server.Migrations.Game
+{
+    public partial class AddingUseActiveSpellCooldown : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UseActiveSpellCooldown",
+                table: "Items",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseActiveSpellCooldown",
+                table: "Items");
+        }
+    }
+}
